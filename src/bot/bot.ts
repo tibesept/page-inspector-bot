067db61bf70930c25ab86c9a7919e601c41c5d0a
mqopi <whoimv2@gmail.com>
2025-10-23 16:08:12 +0700

    refact(bot imports): refact bot imports

--- src/bot/bot.ts
+++ src/bot/bot.ts
@@ -2,19 +2,26 @@ import { Bot, session } from "grammy";
 import { conversations, createConversation } from "@grammyjs/conversations";
 import { FileAdapter } from "@grammyjs/storage-file";
 
-import { config } from "#core/config.js";
 import { loggerMiddleware } from "#core/logger.js";
 import { createAuthMiddleware, devCheckMiddleware } from "#bot/middlewares/auth.js";
 import * as useSession from "#bot/session.js";
 import { ISessionData, TMyContext } from "#types/state.js";
+
+// ===== HANDLERS =====
 import { errorHandler } from "#bot/handlers/error/index.js";
+// commands
 import { basicCommands } from "#bot/handlers/commands/basicCommands.js";
+// conversations
 import { newJob } from "#bot/handlers/conversations/index.js";
-import { ApiService } from "#api/ApiService.js"; // тип
-import { JobService } from "#services/JobService.js";
+
+// ===== API ===== (используем только как типы)
+// repositories
 import { UsersRepository } from "#repositories/UsersRepository.js";
+// services
+import { JobService } from "#services/JobService.js";
 import { UserService } from "#services/UserService.js";
 import { createInjectServices } from "./middlewares/injectServices.js";
+
 
 export function configureBot(
     bot: Bot<TMyContext>,
