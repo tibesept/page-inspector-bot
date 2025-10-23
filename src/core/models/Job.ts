1a2064c5685c372a25fecdf536f162d4e8935663
mqopi <whoimv2@gmail.com>
2025-10-23 22:34:45 +0700

    feat(models): add AI summary support for models; add useful JobStatus hard string type

--- src/core/models/Job.ts
+++ src/core/models/Job.ts
@@ -1,7 +1,8 @@
 import { JobWorkerResultDTO } from "#api/types.js"; // Мы можем переиспользовать тип результата
 import { IAnalyzerSettings } from "#types/state.js";
 
-export type JobStatus = "pending" | "processing" | "completed" | "sent" | "failed";
+// export type JobStatus = "pending" | "processing" | "completed" | "sent" | "failed";
+export type JobStatus = "pending" | "ready" | "failed" | "sent" | "summary_sent";
 
 /**
  * Доменная модель джобы
@@ -13,12 +14,20 @@ export interface Job {
     url: string | null;
     status: JobStatus;
     
+    ai_summary: string | null;
     /**
      * Результат выполнения задачи, уже распарсенный из JSON-строки.
      * Может быть null, если задача еще не выполнена.
      */
     result: JobWorkerResultDTO | null;
+    settings: IAnalyzerSettings | null;
 }
+
+export interface Ready {
+    readyJobs: Job[],
+    readySummaries: AiSummary[]
+}
+
 
 export interface CreateJobParams {
     userId: number;
@@ -30,5 +39,13 @@ export interface CreateJobParams {
         seo: boolean;
         lighthouse: boolean;
         techstack: boolean;
+        ai_summary: boolean;
     };
+}
+
+export interface AiSummary {
+    jobId: number;
+    userId: number;
+    url: string;
+    ai_summary: string;
 }
